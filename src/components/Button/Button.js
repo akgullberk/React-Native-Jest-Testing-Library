97fe55f4ee10e973a63c8a78c6b12a5e9c16b8bd
Berk Akgül <berkak_gul51@hotmail.com>
2024-08-17 18:15:09 +0300

    test snapshot

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+
+const Button = () => {
+  return (
+    <TouchableOpacity>
+        <Text>asd</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({})
